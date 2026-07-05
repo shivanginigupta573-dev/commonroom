e07cef34b60aa7bbbc6ce6ad219d4d38bf03506a
Shivangini Gupta <shivanginigupta573@gmail.com>
2026-07-05 11:47:08 +0530

    Feat: Enable SSL mode for database connection

--- commonroom-backend/commonroom_backend/settings.py
+++ commonroom-backend/commonroom_backend/settings.py
@@ -88,9 +88,11 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT'),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
