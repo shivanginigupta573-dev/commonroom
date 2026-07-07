e3e0a16c0c5cf6645e81f03c3b4e92777e1f0c35
Shivangini Gupta <shivanginigupta573@gmail.com>
2026-07-07 23:53:31 +0530

    fixed deployment issue

--- commonroom-backend/commonroom_backend/settings.py
+++ commonroom-backend/commonroom_backend/settings.py
@@ -80,20 +80,24 @@ WSGI_APPLICATION = 'commonroom_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+DB_SSLMODE = os.getenv(
+    "DB_SSLMODE",
+    "require" if not DEBUG else "prefer"
+)
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-        'OPTIONS': {
-            'sslmode': os.environ.get('DB_SSLMODE', 'disable'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+        "OPTIONS": {
+            "sslmode": DB_SSLMODE,
         },
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
